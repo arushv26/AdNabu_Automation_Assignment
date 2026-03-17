65feb40347c549cc982ff2320ab96f8ec3dab917
Arush Vashishtha <arushvashishtha083@gmail.com>
2026-03-17 13:45:07 +0530

    Refactor add to cart test structure
    Refactor test_add_to_cart.py to improve structure and readability by adding helper functions for setup, login, and product selection.

--- test_add_to_cart.py
+++ test_add_to_cart.py
@@ -7,63 +7,71 @@ from selenium.webdriver.support import expected_conditions as EC
 URL = "https://adnabu-store-assignment1.myshopify.com"
 PASSWORD = "AdNabuQA"
 
-
-def test_search_and_add_to_cart():
-
+def setup_driver():
     service = Service("./chromedriver")
     driver = webdriver.Chrome(service=service)
-    wait = WebDriverWait(driver, 15)
-
-    driver.get(URL)
     driver.maximize_window()
+    wait = WebDriverWait(driver, 15)
+    return driver, wait
 
+def login(driver, wait):
+    driver.get(URL)
     password = wait.until(
         EC.visibility_of_element_located((By.NAME, "password"))
     )
     password.send_keys(PASSWORD)
-
     driver.find_element(By.CSS_SELECTOR, "button[type='submit']").click()
 
+def search_product(driver, wait, product_name):
     search_icon = wait.until(
         EC.element_to_be_clickable((By.CSS_SELECTOR, 'summary[aria-label="Search"]'))
     )
     search_icon.click()
-
     search_box = wait.until(
         EC.visibility_of_element_located((By.NAME, "q"))
     )
-    search_box.send_keys("Snowboard")
+    search_box.send_keys(product_name)
     search_box.submit()
 
     wait.until(EC.url_contains("/search"))
 
+def select_first_product(driver, wait):
     wait.until(
         EC.presence_of_element_located((By.CSS_SELECTOR, ".card-wrapper"))
     )
-
     products = wait.until(
         EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'a[href*="/products/"]'))
     )
-
     driver.execute_script("arguments[0].click();", products[0])
 
+def add_to_cart(driver, wait):
     add_button = wait.until(
         EC.element_to_be_clickable((By.NAME, "add"))
     )
     add_button.click()
 
+def verify_added_to_cart(driver, wait, expected_count="1"):
     cart = wait.until(
         EC.visibility_of_element_located((By.CSS_SELECTOR, ".cart-count-bubble"))
     )
+    wait.until(lambda d: expected_count in cart.text)
 
-    wait.until(lambda d: "1" in cart.text)
 
-    print("Test Passed: Product successfully added to cart")
-
-    driver.implicitly_wait(80)
-
+def driver_quit(driver):
     driver.quit()
+
+def test_add_to_cart():
+    driver, wait = setup_driver()
+
+    login(driver, wait)
+    search_product(driver, wait, "Snowboard")
+    select_first_product(driver, wait)
+    add_to_cart(driver, wait)
+    verify_added_to_cart(driver, wait, "1")
+    print("Test Passed: Product added to the cart")
+
+    driver_quit(driver)
 
 
 if __name__ == "__main__":
-    test_search_and_add_to_cart()
+    test_add_to_cart()
